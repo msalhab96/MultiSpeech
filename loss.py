e14496e2df5921cb6df1842472a6cb8d2253b9ed
Mahmoud Salhab <mahmoud@salhab.work>
2022-06-19 09:56:12 +0300

    diagonal constraints calculation optimized

--- loss.py
+++ loss.py
@@ -1,10 +1,9 @@
 from typing import List, Union
 from torch import Tensor
-import torch.nn as nn
 import torch
 
 
-class Loss(nn.Module):
+class Loss(torch.nn.Module):
     def __init__(
             self,
             h: int,
@@ -12,10 +11,11 @@ class Loss(nn.Module):
             dc_bandwidth: int,
             stop_weight: Union[float, int]
             ) -> None:
+        super().__init__()
         self.h = h
         self.dc_strength = dc_strength
-        self.l1_loss = nn.L1Loss()
-        self.bce = nn.BCELoss()
+        self.l1_loss = torch.nn.L1Loss()
+        self.bce = torch.nn.BCELoss()
         self.stop_weight = stop_weight
         self.dc_bandwidth = dc_bandwidth
 
@@ -30,44 +30,46 @@ class Loss(nn.Module):
             wieght = target * self.stop_weight
             target = target.to(stop_pred.device)
             wieght = wieght.to(stop_pred.device)
-            total = total + nn.BCELoss(
+            total = total + torch.nn.BCELoss(
                 weight=wieght,
                 reduction='sum'
                 )(p_preds, target)
         return total / mask.sum()
 
-    def _compute_dc(self, alignment: Tensor, lengths):
-        bh, s, t = alignment.shape
+    def calc_diagonal_constraint(
+            self, alignments: List[Tensor], lengths: Tensor
+            ) -> Tensor:
+        alignments = torch.stack(alignments, dim=0)
+        n, bh, s, t = alignments.shape
         batch_size = bh // self.h
-        alignment = alignment.view(self.h, batch_size, s, t)
-        alignment = alignment.permute(1, 0, 2, 3)
+        alignments = alignments.view(n, self.h, batch_size, s, t)
+        alignments = alignments.permute(0, 2, 1, 3, 4)
+        # As the data will be sorted by length, we assume all the batch items
+        # approximately has the same length so k constant across all the batch
+        # items
         k = t // s
         time_range = torch.arange(0, t)
+        min_indices = torch.clip(
+            k * time_range - self.dc_bandwidth, min=0, max=s
+            ).long()
+        max_indices = torch.clip(
+            k * time_range + self.dc_bandwidth, min=0, max=s
+            ).long()
         result = 0
         for i in range(t):
-            min_idx = max(0, k * time_range - self.dc_bandwidth)
-            max_idx = min(s, k * time_range + self.dc_bandwidth)
+            min_idx = min_indices[i].item()
+            max_idx = max_indices[i].item()
             mask = t >= lengths
-            mask = mask.view(-1, 1, 1, 1)
-            mask = mask.to(alignment.device)
-            total = alignment[..., min_idx:max_idx, i:i+1] * mask
+            mask = mask.view(1, -1, 1, 1, 1)
+            mask = mask.to(alignments.device)
+            total = alignments[..., min_idx:max_idx, i:i+1] * mask
             result = result + total.sum()
+        result = result / (self.h * lengths.sum() * n)
         return result
-
-    def calc_diagonal_constraint(
-            self,
-            alignments: List[Tensor],
-            lengths: Tensor
-            ) -> Tensor:
-        total = 0
-        for alignment in alignments:
-            total = total + self._compute_dc(alignment, lengths)
-        total = total / (self.h * lengths.sum())
-        return total
 
     def forward(
             self,
-            length: Tensor,
+            lengths: Tensor,
             mask: Tensor,
             stop_pred: Tensor,
             mels_pred: Tensor,
@@ -78,7 +80,9 @@ class Loss(nn.Module):
             mels_pred * mask.unsqueeze(dim=-1), mels_target
             )
         stop_loss = self.calc_bce_loss(mask, stop_pred)
-        diagonal_constraint = self.calc_diagonal_constraint(alignments, length)
+        diagonal_constraint = self.calc_diagonal_constraint(
+            alignments, lengths
+            )
         total_loss = mel_loss + stop_loss
         total_loss = total_loss - self.dc_strength * diagonal_constraint
         return total_loss
